e163e16fdcfdf5a01688fa9128e941fbc0e4b164
Szymon Sobik <szymon.sobik@flytronic.pl>
2016-08-22 16:08:44 +0200

    C library deb contents calculation makes GetContentsFromDeb faster - #276 requires libarchive

--- deb/deb.go
+++ deb/deb.go
@@ -2,16 +2,12 @@ package deb
 
 import (
 	"archive/tar"
-	"compress/bzip2"
 	"compress/gzip"
 	"fmt"
 	"github.com/mkrautz/goar"
 	"github.com/smira/aptly/utils"
-	"github.com/smira/go-xz"
-	"github.com/smira/lzma"
 	"io"
 	"os"
-	"strings"
 )
 
 // GetControlFileFromDeb reads control file from deb package
@@ -100,74 +96,3 @@ func GetControlFileFromDsc(dscFile string, verifier utils.Verifier) (Stanza, err
 
 }
 
-// GetContentsFromDeb returns list of files installed by .deb package
-func GetContentsFromDeb(packageFile string) ([]string, error) {
-	file, err := os.Open(packageFile)
-	if err != nil {
-		return nil, err
-	}
-	defer file.Close()
-
-	library := ar.NewReader(file)
-	for {
-		header, err := library.Next()
-		if err == io.EOF {
-			return nil, fmt.Errorf("unable to find data.tar.* part in %s", packageFile)
-		}
-		if err != nil {
-			return nil, fmt.Errorf("unable to read .deb archive from %s: %s", packageFile, err)
-		}
-
-		if strings.HasPrefix(header.Name, "data.tar") {
-			var tarInput io.Reader
-
-			switch header.Name {
-			case "data.tar":
-				tarInput = library
-			case "data.tar.gz":
-				ungzip, err := gzip.NewReader(library)
-				if err != nil {
-					return nil, fmt.Errorf("unable to ungzip data.tar.gz from %s: %s", packageFile, err)
-				}
-				defer ungzip.Close()
-				tarInput = ungzip
-			case "data.tar.bz2":
-				tarInput = bzip2.NewReader(library)
-			case "data.tar.xz":
-				unxz, err := xz.NewReader(library)
-				if err != nil {
-					return nil, fmt.Errorf("unable to unxz data.tar.xz from %s: %s", packageFile, err)
-				}
-				defer unxz.Close()
-				tarInput = unxz
-			case "data.tar.lzma":
-				unlzma := lzma.NewReader(library)
-				defer unlzma.Close()
-				tarInput = unlzma
-			default:
-				return nil, fmt.Errorf("unsupported tar compression in %s: %s", packageFile, header.Name)
-			}
-
-			untar := tar.NewReader(tarInput)
-			var results []string
-			for {
-				tarHeader, err := untar.Next()
-				if err == io.EOF {
-					return results, nil
-				}
-				if err != nil {
-					return nil, fmt.Errorf("unable to read .tar archive from %s: %s", packageFile, err)
-				}
-
-				if tarHeader.Typeflag == tar.TypeDir {
-					continue
-				}
-
-				if strings.HasPrefix(tarHeader.Name, "./") {
-					tarHeader.Name = tarHeader.Name[2:]
-				}
-				results = append(results, tarHeader.Name)
-			}
-		}
-	}
-}
---
+++ deb/deb_contents_accel.go
@@ -0,0 +1,149 @@
+// +build opt,cgo
+
+package deb
+
+/*
+#cgo CFLAGS: -std=c99
+#cgo LDFLAGS: -larchive
+#include <stdlib.h>
+#include <time.h>
+#include <archive.h>
+#include <archive_entry.h>
+#include <fnmatch.h>
+#include <stdbool.h>
+
+struct a
+{
+	struct archive *deb, *data;
+	struct archive_entry *deb_entry, *data_entry;
+	void *buffer[10240];
+	const char *err;
+};
+
+ssize_t read_a(struct archive *a, void *data, const void **buff)
+{
+	struct a *internal = (struct a*)data;
+	*buff = internal->buffer;
+	int read = archive_read_data(internal->deb, internal->buffer, 10240);
+	if (read < 0)
+	{
+		return -1;
+	}
+	return read;
+}
+
+struct a* open_archive(char *file, bool *error)
+{
+	struct a *internal = malloc(sizeof(struct a));
+	*error = false;
+	internal->deb = archive_read_new();
+	archive_read_support_format_ar(internal->deb);
+	archive_read_open_filename(internal->deb, file, 10240);
+	free(file);
+	if (!internal->deb)
+	{
+		return 0;
+	}
+	while (archive_read_next_header(internal->deb, &internal->deb_entry) == ARCHIVE_OK)
+	{
+		const char* name = archive_entry_pathname(internal->deb_entry);
+		if (!fnmatch("data.*", name, 0))
+		{
+			internal->data = archive_read_new();
+			archive_read_support_format_tar(internal->data);
+			archive_read_support_filter_all(internal->data);
+			int r = archive_read_open(internal->data, internal, 0, read_a, 0);
+			if (r != ARCHIVE_OK)
+			{
+				internal->err = archive_error_string(internal->data);
+				*error = true;
+				return internal;
+			}
+			return internal;
+		}
+	}
+	return 0;
+}
+
+const char* read_content(struct a *internal, bool *error)
+{
+	int r;
+	*error = false;
+	for (;;)
+	{
+		r = archive_read_next_header(internal->data, &internal->data_entry);
+		if (r == ARCHIVE_EOF)
+		{
+			return 0;
+		}
+		if (r != ARCHIVE_OK)
+		{
+			*error = true;
+			internal->err = archive_error_string(internal->data);
+			return 0;
+		}
+		if (archive_entry_filetype(internal->data_entry) == AE_IFDIR)
+			continue;
+		return archive_entry_pathname(internal->data_entry);
+	}
+}
+
+int close_archive(struct a *internal)
+{
+	if (internal->data)
+	{
+		archive_read_close(internal->data);
+		archive_read_free(internal->data);
+	}
+	if (internal->deb)
+	{
+		archive_read_close(internal->deb);
+		archive_read_free(internal->deb);
+	}
+	free(internal);
+	return 0;
+}
+
+const char *get_error(struct a *internal)
+{
+	return internal->err;
+}
+
+*/
+import "C"
+
+import (
+	"fmt"
+	"strings"
+)
+
+// GetContentsFromDeb returns list of files installed by .deb package
+func GetContentsFromDeb(packageFile string) ([]string, error) {
+	err := C.bool(true);
+	f := C.open_archive(C.CString(packageFile), &err)
+	if f == nil {
+		return nil, fmt.Errorf("Error initializing libarchive")
+	}
+	defer C.close_archive(f)
+	if err {
+		return nil, fmt.Errorf("Error initializing libarchive")
+	}
+
+	var results []string
+	for {
+		arg := C.GoString(C.read_content(f, &err))
+		if err {
+			return nil, fmt.Errorf("Error during libarchive decompress: %s", C.GoString(C.get_error(f)))
+		}
+		if len(arg) == 0 {
+			return results, nil
+		}
+		if strings.HasPrefix(arg, "./") {
+			arg = arg[2:]
+		}
+		results = append(results, arg)
+
+	}
+	return results, nil
+}
+
